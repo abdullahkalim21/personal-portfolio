9233d36e31dd423fe55a0627f49923dc8eae8afb
Abdullah Kaleem <abdullahkalim216@gmail.com>
2024-06-01 00:35:30 +0500

    Dark-theme for Terms & Conditions page

--- forms/contact.php
+++ forms/contact.php
@@ -25,12 +25,12 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
 
-  // $contact->smtp = array(
-  //   'host' => 'gmail.com',
-  //   'username' => 'abdullahkalim216',
-  //   'password' => 'pass',
-  //   'port' => '587'
-  // );
+  $contact->smtp = array(
+    'host' => 'stmpgmail.com',
+    'username' => 'abdullahkalim216@gmail.com',
+    'password' => 'kwagypcvhagohfvo', // kwag ypcv hago hfvo
+    'port' => '587'
+  );
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
